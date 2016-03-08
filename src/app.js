2a1ff54b1db2c626cf0856050b6173bba456585b
Hwan-Joon Choi <hc5duke@gmail.com>
2016-03-08 15:24:22 -0800

    passing tests

--- src/app.js
+++ src/app.js
@@ -2,28 +2,38 @@
   'use strict';
 
   function Bobo() {
-    this.stations = {};
+    this.stations = [];
   }
 
   Bobo.prototype.fetch = function() {
     var calls = [];
+
     for (var st in stations) {
       var station = stations[st];
       var url = station;
-      var promise = fetch(url).then(this.processStation)//.bind(this));
+      var promise = fetch(url).
+        then(this.processStation.bind(this));
       calls.push(promise);
     }
 
     return Promise.all(calls);
   };
 
+  //var _getTagText = function (tagGetter, tag) { return tagGetter(tag)[0].textContent; };
   Bobo.prototype.processStation = function (data) {
     var station = data.getElementsByTagName("station")[0];
-    var name = station.getElementsByTagName("abbr")[0].textContent;
-    console.log(name);
-    //this.stations.push(station);
-    return this;
-    //return data;
+    var etds = {};
+    var etdData = station.getElementsByTagName('etd');
+    console.log('xxxx');
+    // build etds
+    for (var e in etdData) {
+      var d = etdData[e];
+    }
+    this.stations.push({
+      name: station.getElementsByTagName("name")[0].textContent,
+      abbr: station.getElementsByTagName("abbr")[0].textContent,
+      //etds: etds,
+    });
   };
 
   var stations = [
--- test/app_test.js
+++ test/app_test.js
@@ -7,59 +7,18 @@ describe('test', function () {
     fetch.files.powl = './fixtures/powl.xml';
     fetch.files.civc = './fixtures/civc.xml';
     var bobo = new Bobo();
+    var allStations = ['embr', 'mont', 'powl', 'civc'];
     bobo.fetch().then(function () {
-      expect(3).to.eq(3);
-      //console.log(bobo.stations[0].getElementsByTagName("abbr")[0].textContent);
+      expect(bobo.stations.length).to.eq(4);
+      for (var s in bobo.stations) {
+        var station = bobo.stations[s];
+        expect(allStations).to.include(station.abbr.toLowerCase());
+      }
+      //console.log(bobo.stations[0]);
       done();
+    }).catch(function (e) {
+      console.dir(e);
+      done('some conditions were not met');
     });
-    //setTimeout(function () {
-      //done();
-    //}, 500);
   });
 });
-/*
-
-var mocha = require('mocha');
-var sinon = require('sinon');
-var chai = require('chai');
-var expect = chai.expect;
-
-//describe('test', function () {
-  //it('test', function () {
-    //var a = 1;
-    //expect(a).to.eq(1);
-
-
-//after(function () {
-  //jQuery.ajax.restore();
-//});
-
-//it("makes a GET request for todo items", function () {
-    //sinon.stub(jQuery, "ajax");
-    //getTodos(42, sinon.spy());
-
-    //assert(jQuery.ajax.calledWithMatch({ url: "/todo/42/items" }));
-//});
-  //});
-//});
-
-var xhr, requests;
-
-before(function () {
-    xhr = sinon.useFakeXMLHttpRequest();
-    requests = [];
-    xhr.onCreate = function (req) { requests.push(req); };
-});
-
-after(function () {
-    // Like before we must clean up when tampering with globals.
-    xhr.restore();
-});
-
-it("makes a GET request for todo items", function () {
-  var app = require('../../src/app');
-
-  assert.equals(requests.length, 1);
-  assert.match(requests[0].url, "/todo/42/items");
-  });
-  */
