5e694350a0635382e6148d423612e449d0ebf3a6
Hwan-Joon Choi <hc5duke@gmail.com>
2016-03-08 10:02:05 -0800

    initinit

---
+++ tests/unit/app_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var mocha = require('mocha');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('test', function () {
+  it('test', function () {
+    var a = 1;
+    expect(a).to.eq(1);
+  });
+});
